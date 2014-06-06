8df84decc238cc35b6a9ff5d7dcabb06ec5aac56
Eric Hu and Rasheed Abdul-Aziz <pair+ehu+squeedee@pivotallabs.com>
2014-06-06 16:29:03 -0400

    Use new machete repo
    [#71850572]

--- cf_spec/integration/firewall_spec.rb
+++ cf_spec/integration/firewall_spec.rb
@@ -1,12 +1,12 @@
 $: << 'cf_spec'
 require "spec_helper"
 
-describe 'deploying a firewall test app', :null_buildpack do
+describe 'deploying a firewall test app' do
   context "an app that does not access the internet" do
     let(:app_name) { "offline_app" }
 
     it "deploys the app successfully" do
-      Machete.deploy_app(app_name, :null) do |app|
+      Machete.deploy_app(app_name) do |app|
         expect(app.homepage_html).to include "Index of"
       end
     end
@@ -18,13 +18,13 @@ describe 'deploying a firewall test app', :null_buildpack do
     if Machete::BuildpackMode.offline?
 
       it "causes an error when trying to access the internet" do
-        Machete.deploy_app(app_name, :null) do |app|
+        Machete.deploy_app(app_name) do |app|
           expect(app.output).to include "Connection refused"
         end
       end
     else
       it "is in online mode and does not fail" do
-        Machete.deploy_app(app_name, :null) do |app|
+        Machete.deploy_app(app_name) do |app|
           expect(app.homepage_html).to include "Index of"
         end
       end
