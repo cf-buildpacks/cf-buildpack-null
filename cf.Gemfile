ee2e4f9fc627a82afb88be2252429f9fb555dc4f
Tim Johnson <pair+buddhistpirate@pivotallabs.com>
2014-06-25 11:57:38 -0400

    Update to new Machete
    [#73723814]

--- cf.Gemfile
+++ cf.Gemfile
@@ -1,3 +1,3 @@
 source "https://rubygems.org"
 
-gem 'machete', git: 'https://github.com/cf-buildpacks/machete.git', ref: '6c02416eb445dd2ed232e8d1b5789302cde182c9'
+gem 'machete', github: 'cf-buildpacks/machete', branch: 'master'
--- cf_spec/integration/firewall_spec.rb
+++ cf_spec/integration/firewall_spec.rb
@@ -1,34 +1,35 @@
 $: << 'cf_spec'
-require "spec_helper"
+require 'spec_helper'
 
-describe 'deploying a firewall test app' do
-  context "an app that does not access the internet" do
-    let(:app_name) { "offline_app" }
+describe 'deploying a firewall test app', :null_buildpack do
+  subject(:app) { Machete.deploy_app(app_name) }
 
-    it "deploys the app successfully" do
-      Machete.deploy_app(app_name) do |app|
-        expect(app.homepage_html).to include "Index of"
-      end
+  context 'an app that does not access the internet' do
+    let(:app_name) { 'offline_app' }
+
+    it 'deploys the app successfully' do
+      expect(app).to be_running
+      expect(app.homepage_body).to include 'Index of'
+      expect(app.host).not_to have_internet_traffic
     end
   end
 
   context 'an uncached buildpack that accesses the internet' do
     let(:app_name) { 'online_app' }
 
-    context 'in an online environment', if: Machete::BuildpackMode.online? do
-      it 'does not fail' do
-        Machete.deploy_app(app_name) do |app|
-          expect(app.homepage_html).to include 'Index of'
-        end
-      end
-    end
-
     context 'in an offline environment', if: Machete::BuildpackMode.offline? do
       it 'causes an error when trying to access the internet' do
-        Machete.deploy_app(app_name) do |app|
-          expect(app.output).to include 'Connection refused'
-          expect(app.cf_internet_log).to include 'cf-to-internet-traffic'
-        end
+        expect(app.host).to have_internet_traffic
+        expect(app).to have_logged 'Network is unreachable'
+        expect(app).to have_logged 'Staging failed: Buildpack compilation step failed'
+        expect(app).not_to be_running
+      end
+    end
+    
+    context 'in an online environment', if: Machete::BuildpackMode.online? do
+      it 'is in online mode and does not fail' do
+        expect(app).to be_running
+        expect(app.homepage_body).to include 'Index of'
       end
     end
   end
