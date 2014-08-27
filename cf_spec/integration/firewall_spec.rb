b33803c53e3e1e5ae6b68edb21cdd3a817870a6e
Rasheed Abdul-Aziz and Rick Reilly <pair+squeedee+rickreilly@pivotallabs.com>
2014-08-27 10:49:34 -0400

    Bumped to latest Machete

--- cf_spec/integration/firewall_spec.rb
+++ cf_spec/integration/firewall_spec.rb
@@ -3,13 +3,17 @@ require 'spec_helper'
 
 describe 'deploying a firewall test app', :null_buildpack do
   subject(:app) { Machete.deploy_app(app_name) }
+  let(:browser) { Machete::Browser.new(app) }
 
   context 'an app that does not access the internet' do
     let(:app_name) { 'offline_app' }
 
     it 'deploys the app successfully' do
       expect(app).to be_running
-      expect(app).to have_page_body 'Index of'
+      
+      browser.visit_path('/')
+      expect(browser).to have_body 'Index of'
+
       expect(app.host).not_to have_internet_traffic
     end
   end
@@ -29,7 +33,9 @@ describe 'deploying a firewall test app', :null_buildpack do
     context 'in an online environment', if: Machete::BuildpackMode.online? do
       it 'is in online mode and does not fail' do
         expect(app).to be_running
-        expect(app).to have_page_body 'Index of'
+
+        browser.visit_path('/')
+        expect(browser).to have_body 'Index of'
       end
     end
   end
